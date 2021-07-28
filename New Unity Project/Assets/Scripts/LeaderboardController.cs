0cc8a7d9ef9e9add6aab82257bbab31b52ad65bf
hasteovertrample <88079413+hasteovertrample@users.noreply.github.com>
2021-07-28 14:29:30 -0400

    first version finished

---
+++ New Unity Project/Assets/Scripts/LeaderboardController.cs
@@ -0,0 +1,74 @@
+using UnityEngine.UI;
+using LootLocker.Requests;
+using UnityEngine;
+
+public class LeaderboardController : MonoBehaviour
+{
+    public InputField MemberID, PlayerScore;
+    public int ID;
+    int maxScores = 5;
+    public Text[] Entries;
+
+
+    private void Start()
+    {
+        LootLockerSDKManager.StartSession("Player", (response) =>
+        {
+            if (response.success)
+            {
+                Debug.Log("Success");
+            }
+            else
+            {
+                Debug.Log("Failed");
+            }
+        });
+    }
+
+    public void ShowScores()
+    {
+        LootLockerSDKManager.GetScoreList(ID, maxScores, (response) =>
+        {
+            if (response.success)
+            {
+                if (response.success)
+                {
+                    LootLockerLeaderboardMember[] scores = response.items;
+
+                    for(int i = 0; i < scores.Length; i++)
+                    {
+                        Entries[i].text = (scores[i].rank + ".   " + scores[i].score);
+                    }
+
+                    if(scores.Length < maxScores)
+                    {
+                        for(int i = scores.Length; i < maxScores; i++)
+                        {
+                            Entries[i].text = (i + 1).ToString() + ".  none";
+                        }    
+                    }
+                }
+                else
+                {
+                    Debug.Log("Failed");
+                } 
+            }
+        });
+
+    }
+    public void SubmitScore()
+    {
+        LootLockerSDKManager.SubmitScore(MemberID.text, int.Parse(PlayerScore.text), ID, (response) =>
+        {
+            if (response.success)
+            {
+                Debug.Log("Success");
+            }
+            else
+            {
+                Debug.Log("Failed");
+            }
+            return;
+        });
+    }
+}
